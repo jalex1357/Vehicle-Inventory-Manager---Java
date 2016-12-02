5008aa97e242ce16f1b2f75acbcd096b7255c34d
jalex1357 <johnathanalexander3@aol.com>
2016-12-02 11:38:00 -0600

    Add files via upload

---
+++ Inventory Manager SQL Scripts/createLogin.sql
@@ -0,0 +1,31 @@
+create database if not exists login;
+use login;
+drop table if exists users;
+
+CREATE TABLE users(
+	employeeId int(11) NOT NULL AUTO_INCREMENT,
+	username varchar(64) DEFAULT NULL,
+	passcode varchar(64) DEFAULT NULL,
+	firstName varchar(64) DEFAULT NULL,
+	lastName varchar(64) DEFAULT NULL,
+	statusLevel varchar(64) DEFAULT NULL,
+	PRIMARY KEY (employeeId)
+	)ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = latin1;
+
+INSERT INTO users (employeeId, username, passcode, firstName, lastName, statusLevel, accountStatus, employeeStatus, dept)
+	VALUES (1, 'test', 'testaccount', 'Test', 'Account', 'Admin', 'Normal', 'Normal', 'Technology');
+INSERT INTO users (employeeId, username, passcode, firstName, lastName, statusLevel, accountStatus, employeeStatus, dept)
+	VALUES (2, 'johnathan', 'password', 'Johnathan', 'Alexander', 'New Hire', 'Normal', 'Normal', 'Technology');
+    
+
+use login;
+drop table if exists dept;
+
+CREATE TABLE dept (
+	Department varchar(64) DEFAULT NULL
+    );
+    
+use login;
+INSERT INTO dept (Department) VALUES ('Human Resources');
+INSERT INTO dept (Department) VALUES ('Technology');
+INSERT INTO dept (Department) VALUES ('Sales');
---
+++ Inventory Manager SQL Scripts/createPossibleVehicles.sql
@@ -0,0 +1,16 @@
+create database if not exists possiblevehicles;
+use possiblevehicles;
+drop table if exists makes;
+
+CREATE TABLE makes (
+	Make varchar(64) DEFAULT NULL);
+    
+INSERT INTO makes (Make) VALUES ('Honda');
+
+
+drop table if exists models;
+CREATE TABLE models(
+	Model varchar(64) DEFAULT NULL);
+    
+INSERT INTO models (Model) VALUES ('Honda:Odyssey');
+
